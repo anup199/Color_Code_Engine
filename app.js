42506a4d62a1912f74e10db6d54c3a06cd759630
anup199 <anupsingh6625@gmail.com>
2023-11-26 22:46:22 +0530

    project add

---
+++ app.js
@@ -0,0 +1,16 @@
+const getColor = () =>{
+     const randomNumber = Math.floor(Math.random() * 16777215);
+     const randomCode = "#" + randomNumber.toString(16);
+     // console.log(randomCode,randomNumber);
+     document.body.style.backgroundColor = randomCode;
+     document.getElementById("color_code").innerText = randomCode;
+
+     navigator.clipboard.writeText(randomCode);
+}
+
+document.getElementById("btn").addEventListener(
+     "click",
+     getColor
+)
+
+getColor();
